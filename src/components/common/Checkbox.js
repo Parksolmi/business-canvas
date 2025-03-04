5b4edf871dc8c6c66f12143c4417cd155eb23e60
Solmi <7548ck@naver.com>
2025-03-04 16:16:15 +0900

    feat: 체크박스 컴포넌트 생성

---
+++ src/components/common/Checkbox.js
@@ -0,0 +1,58 @@
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const Checkbox = ({ label, checked, onChange }) => {
+  useEffect(() => {
+    console.log(checked);
+  }, [checked]);
+
+  return (
+    <WrapperCheckbox>
+      {label}
+      <input
+        className="hidden-checkbox"
+        type="checkbox"
+        checked={checked}
+        onChange={onChange}
+      />
+      <StyledCheckbox $checked={checked} />
+    </WrapperCheckbox>
+  );
+};
+
+Checkbox.propTypes = {
+  label: PropTypes.string,
+  checked: PropTypes.bool,
+  onChange: PropTypes.func.isRequired,
+};
+
+const WrapperCheckbox = styled.label`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .hidden-checkbox {
+    display: none;
+  }
+`;
+
+const StyledCheckbox = styled.div`
+  display: inline-block;
+  width: 16px;
+  height: 16px;
+  border: 1px solid #e3e3e3;
+  border-radius: 5px;
+  background-color: white;
+  transition: all 0.15s ease-in-out;
+
+  ${({ $checked }) =>
+    $checked &&
+    `
+      background: url(/assets/png/check-icon.png) #4A7CFE no-repeat center;
+      background-size: 10px auto;
+      border-color: transparent;
+    `}
+`;
+
+export default Checkbox;
